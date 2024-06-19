a4ed60388f50cc87873498a87943513036f04f48
Yibo Yan <yiboyan@usc.edu>
2024-06-19 12:49:46 -0700

    Update profile.py

--- profile.py
+++ profile.py
@@ -130,12 +130,15 @@ fsnode = request.RemoteBlockstore("fsnode", "/mydata")
 fsnode.dataset = "urn:publicid:IDN+wisc.cloudlab.us:flashburst-pg0+ltdataset+ray-text-file"
 
 num_fslinks = math.ceil(params.nodeCount / 10)
-fslinks = [ request.Link(f"fslink-{i}") for i in range(num_fslinks) ]
-for fslink in fslinks:
+# fslinks = [ request.Link(f"fslink-{i}") for i in range(num_fslinks) ]
+fslinks = []
+for i in range(num_fslinks):
+  fslink = request.Link(f"fslink-{i}")
   fslink.addInterface(fsnode.interface)
   # Special attributes for this link that we must use.
   fslink.best_effort = True
   fslink.vlan_tagging = True
+  fslinks.append(fslink)
 
 # Process nodes, adding to link.
 for i in range(params.nodeCount):
